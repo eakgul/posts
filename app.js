20b7af59c9cfb4d034890df3fa46804a28e0f580
eakgul <esataakgul@gmail.com>
2023-08-08 15:14:51 +0300

    posts
    JS kullanarak fake api ile post ve comment çekmek.

---
+++ app.js
@@ -0,0 +1,26 @@
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then(response => response.json())
+.then(posts => {
+    const appDiv = document.getElementById('app');
+    posts.forEach(post => {
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('card');
+        cardDiv.innerHTML = `
+            <img src="https://placehold.co/200x200" class="img">
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            <button class="btn" onclick="openDetails(this)">Daha Fazla...</button>
+        `;
+        appDiv.appendChild(cardDiv);
+    });
+})
+.catch(error => console.error(error));
+
+function openDetails(button) {
+const card = button.parentElement;
+const title = card.querySelector('h3').textContent;
+const body = card.querySelector('p').textContent;
+const postData = { title, body };
+const queryParams = new URLSearchParams(postData).toString();
+window.open(`details.html?${queryParams}`, '_blank');
+}
